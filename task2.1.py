b11761f86c03199cedc76631409b58020ed46e00
Advait-Bankar <advaitbankar45@gmail.com>
2025-06-29 22:54:09 +0530

    Add files via upload

---
+++ task2.1.py
@@ -0,0 +1,14 @@
+# Task1 from Module3
+
+number = int(input("Enter a number: "))
+result = number / 2
+
+# Check if result is equal to its int version
+if result == int(result):
+    print("The number is Even.")
+
+else:
+    print("The number is Odd.")
+
+#Closure statement...
+print("\nThanks for using this Program, hope you liked it!!!")
---
+++ task2.2.py
@@ -0,0 +1,8 @@
+#Task2 from module3
+
+total = 0
+
+for number in range(1, 51, 1):
+    total = total + number 
+
+print("The sum of numbers from 1 to 50 is: ", total)
